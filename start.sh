9498ed762084387ec58422a5c7541e16747a83bd
Caleb Godwin <caleb@godwin.io>
2020-11-04 12:54:45 -0500

    start.sh checks for validity of port in config.json

--- start.sh
+++ start.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
+# Build the project
 echo Building Project Purple Cow...
 sudo docker build -t projectpurplecow:latest .
-echo Running Project Purple Cow...
+
+# Retrieve desired port from config file
 PORT=$(python3 -c "import json; print(json.load(open('ProjectPurpleCow/config.json', 'r'))['port'])")
+[ -n "$PORT" ] && [ "$PORT" -eq "$PORT" ] 2>/dev/null # Check if $PORT is a valid number
+if [ $? -ne 0 ]; then
+    echo
+    echo
+    echo Failed to retrieve port from project configuration file. Please check it and try again!
+    exit
+fi
+
+# Run the project
 sudo docker run -d -p $PORT:$PORT projectpurplecow
 echo 
 echo 
-echo ProjectPurpleCow is running. To stop, use \"sudo docker stop [id]\" where [id] is \(at least part of\) the container ID shown above.
+echo Project Purple Cow is running on port $PORT. To stop, use \"sudo docker stop [id]\" where [id] is \(at least part of\) the container ID shown above.
