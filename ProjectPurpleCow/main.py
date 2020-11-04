3424d404e6833d33794fcd905cea39ca896d7396
Caleb Godwin <caleb@godwin.io>
2020-11-04 13:34:08 -0500

    Moved API hit to backend to protect API keys

--- ProjectPurpleCow/main.py
+++ ProjectPurpleCow/main.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import Flask, render_template
 
 from Utilities import get_config
@@ -8,8 +10,34 @@ app = Flask(__name__)
 def index():
     try:
         config = get_config()
-        return render_template("index.html", api_key=config["api_key"])
+        return render_template("index.html")
     except Exception as e:
         return render_template("error.html", error="There was a problem with the application's configuration file.", error_content=str(e))
-        
+
+@app.route("/hit/<string:button_id>")
+def hit(button_id):
+    try:
+        api_key = get_config()["buttons"][button_id]
+    except:
+        return "API key for {} was not found!".format(button_id)
+    
+    try:
+        response = requests.get("https://api.countapi.xyz/hit/{}".format(api_key))
+    except Exception as e:
+        return "Error occured while trying to hit API: {}".format(e)
+
+    if response.status_code != 200 and response.status_code != 400:
+        return "API returned status code {}!".format(response.status_code)
+    
+    try:
+        response_json = response.json()
+    except:
+        return "API returned non-JSON response!"
+
+    if "error" in response_json:
+        return "API returned error: {}".format(response_json["error"])
+    elif "value" in response_json:
+        return "Number of hits is {}.".format(response_json["value"])
+    else:
+        return "Unrecognized response from API!"
     
